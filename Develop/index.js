726075ab89c6421c2d9b9725f3d4b3c55cded9e6
Matthew <mduncan766@gmail.com>
2023-05-06 01:18:17 -0500

    Modifiying the index and markdown files to add the license badge and link to the license section of the readme file.

--- Develop/index.js
+++ Develop/index.js
@@ -11,3 +11,80 @@ function init() {}
 
 // Function call to initialize app
 init();
+// Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// Array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please provide a description of your project:'
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Please provide installation instructions for your project:'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Please provide usage information for your project:'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What license would you like to use for your project?',
+    choices: ['Apache', 'MIT', 'GNU', 'None']
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Please provide contribution guidelines for your project:'
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Please provide test instructions for your project:'
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?'
+  }
+];
+
+// Function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log('README.md created!')
+  );
+}
+
+// Function to initialize app
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      const markdown = generateMarkdown(answers);
+      writeToFile('README.md', markdown);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+// Function call to initialize app
+init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -15,6 +15,89 @@ function generateMarkdown(data) {
   return `# ${data.title}
 
 `;
+}// Function to return a license badge based on which license is passed in
+function renderLicenseBadge(license) {
+  if (!license) {
+    return "";
+  }
+
+  switch (license.toLowerCase()) {
+    case "apache":
+      return "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
+    case "mit":
+      return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+    case "gnu":
+      return "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)";
+    default:
+      return "";
+  }
 }
+
+// Function to return the license link
+function renderLicenseLink(license) {
+  if (!license) {
+    return "";
+  }
+
+  switch (license.toLowerCase()) {
+    case "apache":
+      return "https://opensource.org/licenses/Apache-2.0";
+    case "mit":
+      return "https://opensource.org/licenses/MIT";
+    case "gnu":
+      return "https://www.gnu.org/licenses/gpl-3.0";
+    default:
+      return "";
+  }
+}
+
+// Function to return the license section of README
+function renderLicenseSection(license) {
+  if (!license) {
+    return "";
+  }
+
+  return `
+## License
+This project is licensed under the [${license} license](${renderLicenseLink(license)}).
+  `;
+}
+
+// Function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For any questions about the project, please contact me via email at ${data.email}. You can also find more of my work at [${data.github}](https://github.com/${data.github}/).
+`;
+}
+
+module.exports = generateMarkdown;
+
 
 module.exports = generateMarkdown;
